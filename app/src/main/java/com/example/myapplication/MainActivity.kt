84c33db6ba78b8171dfd97a788499f708cf2b8fa
sabdahtb <sabdahutabarat2804@gmail.com>
2023-04-08 17:39:43 +0700

    refactory workshop

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnBelajar.setOnClickListener {
+            Toast.makeText(this, "ini adalah toast", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
